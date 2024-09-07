2cfef085319926fa7dc4ef01806e0791b0b36882
gupta-harshal <harshal.2023ug3013@iiitranchi.ac.in>
2024-09-07 12:48:58 +0530

    Added Chatbot

--- TripBackend/apis/gemgpt.js
+++ TripBackend/apis/gemgpt.js
@@ -1,6 +1,10 @@
 const axios=require('axios')
+const env=require('dotenv')
+env.config();
+const readline = require('readline');
+
 url='https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyCXSAY_XaH_Ut_SdWg-nLwYVxyzNsjQKIc'
-async function generateAnswer()
+async function generateAnswer(question)
 {
     const response=await axios({
         url:url,
@@ -8,11 +12,29 @@ async function generateAnswer()
         data:{
             contents:[
                 {parts:[{
-                    text:"How is Hong Kong"
+                    text:question
                 }]}
             ]
         }
     });
-    console.log(response['data']['candidates'][0]['content'])
+    console.log(response['data']['candidates'][0]['content']['parts'][0]['text'])
 }
-generateAnswer()
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Function to handle the conversation loop
+function startConversation() {
+    rl.question("You: ", (userInput) => {
+        if (userInput.toLowerCase() === 'bye') {
+            console.log("Goodbye!");
+            rl.close();
+        } else {
+            generateAnswer(userInput).then(() => {
+                startConversation();n
+            });
+        }
+    });
+}
+startConversation();
